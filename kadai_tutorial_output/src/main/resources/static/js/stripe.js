5733ff20d9f75aeb0c69dc9771674246f7bf7850
Ryo23sanyu <147248868+Ryo23sanyu@users.noreply.github.com>
2023-11-13 23:16:04 +0900

    Delete kadai_tutorial_output directory

--- kadai_tutorial_output/src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51O9Oi2EiYO09PcsXvq1zXwGPU5naWIUouvudyrUTkzQahbI8o6VUUOixP5rziRY3A0Enq2qZuNx8Bl3jhbhYiIuD0058ZLcWkk');
- const paymentButton = document.querySelector('#paymentButton');
- 
- paymentButton.addEventListener('click', () => {
-   stripe.redirectToCheckout({
-     sessionId: sessionId
-   })
- });
